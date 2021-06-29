1c8aaf3a8f101a551f475861d143387020894fc9
shellwe <shellwe@gmail.com>
2021-06-29 16:11:55 -0500

    going to reset node packages

--- gulpfile.js
+++ gulpfile.js
@@ -1,51 +1,15 @@
 'use strict';
 
 var gulp = require('gulp');
-var sass = require('gulp-sass')(require('sass'));
+var sass = require('gulp-dart-sass');
 
-function buildStyles() {
+gulp.task('sass', function () {
   return gulp.src('scss/styles.scss')
-    .pipe(sass().on('error', sass.logError))
+    .pipe(sass.sync().on('error', sass.logError))
     .pipe(gulp.dest('css'));
-};
+});
 
-exports.buildStyles = buildStyles;
-exports.watch = function () {
-  gulp.watch('scss/*.scss', ['sass']);
-};
-
-// var gulp = require('gulp');
-// var sass = require('gulp-sass');
-// gulp.task('sass', function () {
-//     return gulp.src('scss/styles.scss')
-//         .pipe(sass()) // Converts Sass to CSS with gulp-sass
-//         .pipe(gulp.dest(''))
-// });
-
-
-
-
-// const gulp = require('gulp');
-// const sass = require('gulp-sass');
-
-// gulp.task('styles', () => {
-//     return gulp.src('scss/styles.scss')
-//         .pipe(sass().on('error', sass.logError))
-//         .pipe(gulp.dest(''));
-// });
-
-// 'use strict';
-
-// var gulp = require('gulp');
-// var sass = require('gulp-sass')(require('sass'));
-
-// function buildStyles() {
-//   return gulp.src('./sass/**/*.scss')
-//     .pipe(sass().on('error', sass.logError))
-//     .pipe(gulp.dest('./css'));
-// };
-
-// exports.buildStyles = buildStyles;
-// exports.watch = function () {
-//   gulp.watch('./sass/**/*.scss', ['sass']);
-// };
+gulp.task('sass:watch', function () {
+    gulp.watch('./sass/**/*.scss', ['sass']);
+  });
+  

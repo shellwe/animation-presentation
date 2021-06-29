f34581e3a04231dc305aabe5bc2bcfff702ec66f
shellwe <shellwe@gmail.com>
2021-06-29 16:12:12 -0500

    missed one

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,28 @@
-'use strict';
+// const gulp = require('gulp');
+// const sass = require('gulp-dart-sass');
+// const autoprefixer = require('gulp-autoprefixer');
 
-var gulp = require('gulp');
-var sass = require('gulp-dart-sass');
+// exports.default = () => (
+// 	gulp.src('css/styles.css')
+// 		.pipe(autoprefixer({
+// 			cascade: false
+// 		}))
+// 		.pipe(gulp.dest('dist'))
+// );
 
-gulp.task('sass', function () {
-  return gulp.src('scss/styles.scss')
-    .pipe(sass.sync().on('error', sass.logError))
-    .pipe(gulp.dest('css'));
-});
 
-gulp.task('sass:watch', function () {
-    gulp.watch('./sass/**/*.scss', ['sass']);
-  });
+// 'use strict';
+
+// var gulp = require('gulp');
+// var sass = require('gulp-dart-sass');
+
+// gulp.task('sass', function () {
+//   return gulp.src('scss/styles.scss')
+//     .pipe(sass.sync().on('error', sass.logError))
+//     .pipe(gulp.dest('css'));
+// });
+
+// gulp.task('sass:watch', function () {
+//     gulp.watch('./sass/**/*.scss', ['sass']);
+//   });
   

475b7f003df5c84066d1adbeedadf878d9944186
shellwe <shellwe@gmail.com>
2021-07-13 01:28:14 -0500

    updated slides and added SVG animation/lottie

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,69 @@
-$(document).ready(function () {
+// $(document).ready(function () {
 
-    function isScrolledIntoView(elem) {
-        var docViewTop = $(window).scrollTop();
-        var docViewBottom = docViewTop + $(window).height();
+//     function isScrolledIntoView(elem) {
+//         var docViewTop = $(window).scrollTop();
+//         var docViewBottom = docViewTop + $(window).height();
 
-        var elemTop = $(elem).offset().top;
-        var elemBottom = elemTop + $(elem).height();
+//         var elemTop = $(elem).offset().top;
+//         var elemBottom = elemTop + $(elem).height();
 
-        return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));
-    }
+//         return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));
+//     }
 
-    function scrollEvents() {
-        $(".slide").each(function () {
-            console.info($(this));
-            if (isScrolledIntoView($(this))) {
-                $(this).addClass("isthisworking");
-            }
-        });
-    }
+//     function scrollEvents() {
+//         $(".slide").each(function () {
+//             if (isScrolledIntoView($(this))) {
+//                 $(this).addClass("isthisworking");
+//             }
+//         });
+//     }
 
-    scrollEvents();
-    window.addEventListener('scroll', scrollEvents);
-    window.addEventListener('resize', scrollEvents);
+//     scrollEvents();
+//     // window.addEventListener('scroll', scrollEvents);
+//     window.addEventListener('scroll', scrollEvents);
+//     window.addEventListener('resize', scrollEvents);
 
+// });
+
+// ----- Custom JS and click events  ----- 
+$(".toggle-animation").click(function() {
+    $( this ).toggleClass( "stopAnimation" );
+  });
+
+  $(".flipping-text-trigger").click(function() {
+    $( this ).toggleClass( "flipping-text" );
+  });
+
+  
+
+// ----- GSAP MotionPath example ----- 
+//register the plugin (just once)
+gsap.registerPlugin(MotionPathPlugin);
+
+var tween = gsap.to("#box", {
+  motionPath: {
+    path: "#path",
+    align: "#path",
+    alignOrigin: [0.5, 0.5], // <-- play with these numbers
+    autoRotate: true
+  },
+  duration: 5, 
+  ease: "power1.inOut",
+  repeat: 100,
+  yoyo: true
 });
+
+// to make it responsive on window resize...
+window.addEventListener("resize", function() {
+  var time = tween.totalTime();
+  tween.seek(0).invalidate().totalTime(time);
+});
+
+// ----- Lottie example ----- 
+var animatoin = bodymovin.loadAnimation({
+    container: document.getElementById('lottieAnimation'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: 'js/data.json'
+})
